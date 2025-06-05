4c64639a4f8d530ea3c4b21fcbdee0de363aee11
Nino <Anthonydavidtabet@gmail.com>
2025-06-05 15:39:38 +0700

    added X402 section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,7 @@ import { Layout121 } from "./components/Layout121";
 import { Blog44 } from "./components/Blog44";
 import { Cta25 } from "./components/Cta25";
 import { Header47 } from "./components/Header47";
+import { HeaderX402 } from "./components/HeaderX402";
 import { Contact23 } from "./components/Contact23";
 
 export default function Page() {
@@ -18,6 +19,7 @@ export default function Page() {
       <Blog44 />
       <Cta25 />
       <Header47 />
+      <HeaderX402 />
       <Contact23 />
     </div>
   );
