f32239a74b085e155214771b0a6f4c84c3697f13
vigilante-d-e-v <vigilantedev@proton.me>
2025-07-20 23:26:28 -0700

    refactor away from relume

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Navbar1 } from "./components/Navbar1";
 import { Header1 } from "./components/Header1";
-import { Layout91 } from "./components/Layout91";
-import { Layout121 } from "./components/Layout121";
-import { Blog44 } from "./components/Blog44";
+import { AgentMarketplace } from "./components/AgentMarketplace";
+import { HowItWorks } from "./components/HowItWorks";
+import { UseCases } from "./components/UseCases";
 import { Cta25 } from "./components/Cta25";
 import { Header47 } from "./components/Header47";
 import { HeaderX402 } from "./components/HeaderX402";
@@ -14,9 +14,9 @@ export default function Page() {
     <div>
       <Navbar1 />
       <Header1 />
-      <Layout91 />
-      <Layout121 />
-      <Blog44 />
+      <AgentMarketplace />
+      <HowItWorks />
+      <UseCases />
       <Cta25 />
       <Header47 />
       <HeaderX402 />
