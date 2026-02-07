a23d5e9c4ef40a260494674a7b6f8bb56f9509c0
Ariq Fadhil Musyaffa <ariqfms@gmail.com>
2026-02-07 10:15:24 +0700

    style: adjust Read Blog button position

--- src/components/sections/Blog.jsx
+++ src/components/sections/Blog.jsx
@@ -23,101 +23,105 @@ export const Blog = async () => {
               Read up on updates, tutorials, and actionable tips.
             </p>
           </div>
-          <Link
-            className="inline-flex items-center justify-center bg-[linear-gradient(90deg,#4D63F6_17%,#1D45D8_65%)] text-white px-4 py-2.5 mt-4 text-sm font-medium shadow-[inset_0_0_0_1px_rgba(255,255,255,0.2)] rounded-lg transition-colors hover:bg-[#FFFFFF]"
-            href={process.env.NEXT_PUBLIC_BLOG_PAYAI_NETWORK || "#"}
-            target="_blank"
-          >
-            Read Blog
-            <ArrowRight className="w-5 h-5 ml-2" />
-          </Link>
         </div>
       </div>
       <div className="space-y-6 lg:space-y-[60px]">
-       {featured && (
-        <div className="border-y border-[#EDEDED]">
-          <div className="container-payai grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 border-x border-[#E4E4E7] bg-white lg:h-[520px] w-full">
-            <div className="border border-[#EDEDED]   px-4 lg:px-8 py-6 lg:py-10 w-full flex flex-col justify-between gap-8">
-              <div>
-                <span className="text-sm lg:text-base text-[#1D45D8] font-medium">
-                  {featured.tags?.[0]?.name}
-                </span>
-                <h3 className="text-2xl lg:text-[32px] lg:leading-[46px] font-medium text-[#09090B] mt-2 lg:mt-3">
-                  {featured.title}
-                </h3>
-                <p className="text-sm lg:text-base text-[#71717A] mt-3 lg:mt-6">
-                  {featured.excerpt}
-                </p>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  {featured.authors?.[0]?.profile_image && (
-                    <Image
-                      src={featured.authors[0].profile_image}
-                      alt={featured.authors[0].name}
-                      width={26}
-                      height={26}
-                      className="w-6 h-6 rounded-full object-cover"
-                    />
-                  )}
-                  <p className="text-[13px] lg:text-sm text-[#0A0A0A]/60">
-                    {featured.authors?.[0]?.name}
+        {featured && (
+          <div className="border-y border-[#EDEDED]">
+            <div className="container-payai grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 border-x border-[#E4E4E7] bg-white lg:h-[520px] w-full">
+              <div className="border border-[#EDEDED]   px-4 lg:px-8 py-6 lg:py-10 w-full flex flex-col justify-between gap-8">
+                <div>
+                  <span className="text-sm lg:text-base text-[#1D45D8] font-medium">
+                    {featured.tags?.[0]?.name}
+                  </span>
+                  <h3 className="text-2xl lg:text-[32px] lg:leading-[46px] font-medium text-[#09090B] mt-2 lg:mt-3">
+                    {featured.title}
+                  </h3>
+                  <p className="text-sm lg:text-base text-[#71717A] mt-3 lg:mt-6">
+                    {featured.excerpt}
                   </p>
                 </div>
-                <p className="text-[13px] lg:text-sm text-[#0A0A0A]/60">
-                  {featured.published_at
-                    ? new Date(featured.published_at).toLocaleDateString()
-                    : ""}
-                </p>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-2">
+                    {featured.authors?.[0]?.profile_image && (
+                      <Image
+                        src={featured.authors[0].profile_image}
+                        alt={featured.authors[0].name}
+                        width={26}
+                        height={26}
+                        className="w-6 h-6 rounded-full object-cover"
+                      />
+                    )}
+                    <p className="text-[13px] lg:text-sm text-[#0A0A0A]/60">
+                      {featured.authors?.[0]?.name}
+                    </p>
+                  </div>
+                  <p className="text-[13px] lg:text-sm text-[#0A0A0A]/60">
+                    {featured.published_at
+                      ? new Date(featured.published_at).toLocaleDateString()
+                      : ""}
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="p-4 border lg:border-l border-[#E4E4E7]">
-              <div className="relative w-full h-full">
-                {featured.feature_image && (
-                  <Image
-                    src={featured.feature_image}
-                    alt={featured.title}
-                    fill
-                    className="object-cover"
-                  />
-                )}
+              <div className="p-4 border lg:border-l border-[#E4E4E7]">
+                <div className="relative w-full h-full">
+                  {featured.feature_image && (
+                    <Image
+                      src={featured.feature_image}
+                      alt={featured.title}
+                      fill
+                      className="object-cover"
+                    />
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-       )}
-        <div className="border-y border-[#EDEDED]">
+        )}
+        <div
+          className={others.length > 0 ? "border-y border-[#E4E4E7]" : "hidden"}
+        >
           <div className="container-payai grid grid-cols-1 lg:grid-cols-3 border-x border-[#E4E4E7] bg-white lg:h-[360px] w-full">
             {others.map((post) => (
               <div
                 key={post.id}
                 className="px-4 lg:px-8 py-6 lg:py-10 w-full flex flex-col justify-between border border-[#EDEDED] gap-8"
               >
-              <div>
-                <span className="text-[#1D45D8] font-medium">
-                  {post.tags?.[0]?.name}
-                </span>
-                <h4 className="text-2xl lg:text-[28px] lg:leading-[40px] font-medium text-[#09090B] mt-2 lg:mt-3 line-clamp-2">
-                  {post.title}
-                </h4>
-                <p className="text-sm lg:text-base text-[#71717A] mt-3 lg:mt-6 line-clamp-3">
-                  {post.excerpt}
-                </p>
+                <div>
+                  <span className="text-[#1D45D8] font-medium">
+                    {post.tags?.[0]?.name}
+                  </span>
+                  <h4 className="text-2xl lg:text-[28px] lg:leading-[40px] font-medium text-[#09090B] mt-2 lg:mt-3 line-clamp-2">
+                    {post.title}
+                  </h4>
+                  <p className="text-sm lg:text-base text-[#71717A] mt-3 lg:mt-6 line-clamp-3">
+                    {post.excerpt}
+                  </p>
+                </div>
+                <div className="flex items-center justify-between">
+                  <p className="text-[13px] lg:text-sm text-[#0A0A0A]/60">
+                    {post.authors?.[0]?.name}
+                  </p>
+                  <p className="text-[13px] lg:text-sm text-[#0A0A0A]/60">
+                    {featured.published_at
+                      ? new Date(featured.published_at).toLocaleDateString()
+                      : ""}
+                  </p>
+                </div>
               </div>
-              <div className="flex items-center justify-between">
-                <p className="text-[13px] lg:text-sm text-[#0A0A0A]/60">
-                  {post.authors?.[0]?.name}
-                </p>
-                <p className="text-[13px] lg:text-sm text-[#0A0A0A]/60">
-                  {featured.published_at
-                    ? new Date(featured.published_at).toLocaleDateString()
-                    : ""}
-                </p>
-              </div>
-            </div>
-          ))}
+            ))}
           </div>
         </div>
+      </div>
+      <div className="container-payai flex justify-center mt-8 lg:mt-16">
+        <Link
+          className="inline-flex items-center justify-center bg-[linear-gradient(90deg,#4D63F6_17%,#1D45D8_65%)] text-white px-4 py-2.5 text-sm font-medium shadow-[inset_0_0_0_1px_rgba(255,255,255,0.2)] rounded-lg transition-colors hover:bg-[#FFFFFF]"
+          href={process.env.NEXT_PUBLIC_BLOG_PAYAI_NETWORK || "#"}
+          target="_blank"
+        >
+          Read Blog
+          <ArrowRight className="w-5 h-5 ml-2" />
+        </Link>
       </div>
     </section>
   );
